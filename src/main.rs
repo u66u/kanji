7fa6649b022fcff0ec3cdff2e054b3087d396796
technicca <cake3327@gmail.com>
2023-12-07 21:29:09 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 extern crate rand;
 extern crate serde;
 extern crate colored;
+extern crate crossterm;
 
 use rand::Rng;
 use serde::Deserialize;
 use colored::*;
+use crossterm::{
+    event::{ read, Event, KeyCode },
+    execute,
+    terminal::{ disable_raw_mode, enable_raw_mode },
+};
+use std::io;
 
 #[derive(Deserialize, Debug)]
 struct Kanji {
@@ -15,26 +22,56 @@ struct Kanji {
     meaning: String,
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let kanji_list: Vec<Kanji> = serde_json::from_str(include_str!("kanji.json")).unwrap();
     let mut rng = rand::thread_rng();
-    let kanji = &kanji_list[rng.gen_range(0..=kanji_list.len())];
+    let kanji = &kanji_list[rng.gen_range(0..kanji_list.len())];
 
-    println!("Do you know this kanji? (Y/N) {}", kanji.character);
+    println!("{}", kanji.character.repeat(3));
+    println!("{}", kanji.character.repeat(3));
+    println!("{}", kanji.character.repeat(3));
+    println!("Onyomi: {}", kanji.onyomi);
+    println!("Kunyomi: {}", kanji.kunyomi);
 
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+    println!("Do you know this kanji? (Y/N)");
 
-    if input.trim() == "yes" {
-        println!("What is the meaning of this kanji?");
-        input.clear();
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        if input.trim() == kanji.meaning {
-            println!("{}", "Correct!".green());
-        } else {
-            println!("{}", "Incorrect.".red());
-            println!("The correct meaning is: {}", kanji.meaning);
+    enable_raw_mode()?;
+    let mut response = ' ';
+    while let Event::Key(event) = read()? {
+        match event.code {
+            KeyCode::Char('y') | KeyCode::Char('Y') => {
+                response = 'y';
+                break;
+            }
+            KeyCode::Char('n') | KeyCode::Char('N') => {
+                response = 'n';
+                break;
+            }
+            _ => {
+                continue;
+            }
         }
     }
+    disable_raw_mode()?;
+
+    match response {
+        'y' => {
+            println!("What is the meaning of this kanji?");
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+
+            if input.trim().eq_ignore_ascii_case(&kanji.meaning) {
+                println!("{}", "Correct!".green());
+            } else {
+                println!("{}", "Incorrect.".red());
+                println!("The correct meaning is: {}", kanji.meaning);
+            }
+        }
+        'n' => {
+            println!("The meaning of this kanji is: {}", kanji.meaning);
+        }
+        _ => {}
+    }
+
+    Ok(())
 }
