c2f5835791fa9c2352ef749d6dab0b5a80b360ec
technicca <cake3327@gmail.com>
2023-12-08 03:13:29 +0300

    ability to choose jlpt levels

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,21 @@ extern crate colored;
 extern crate crossterm;
 extern crate open;
 
-use std::fs::File;
-use std::io::Write;
+use std::env::{ self };
+use std::fs::{ self, File };
+use std::io::{ self, Write, Read };
 use rand::Rng;
-use serde::Deserialize;
+use serde::{ Deserialize, Serialize };
 use colored::*;
 use crossterm::{ event::{ read, Event, KeyCode }, terminal::{ disable_raw_mode, enable_raw_mode } };
-use std::io;
 
-#[derive(Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Config {
+    selected_category: Option<String>,
+    kanji: Vec<Kanji>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
 struct Kanji {
     category: String,
     character: String,
@@ -48,15 +54,54 @@ fn show_kanji(character: &str) -> std::io::Result<()> {
 }
 
 fn main() -> std::io::Result<()> {
-    let kanji_list: Vec<Kanji> = serde_json::from_str(include_str!("kanji.json")).unwrap();
-    let mut rng = rand::thread_rng();
-    let kanji = &kanji_list[rng.gen_range(0..kanji_list.len())];
+    let args: Vec<String> = env::args().collect();
+    let selected_category_arg = args.get(1);
 
-    println!("Kanji: {}", kanji.character);
-    println!("Onyomi: {}", kanji.onyomi);
-    println!("Kunyomi: {}", kanji.kunyomi);
+    let mut file = File::open("kanji.json")?;
+    let mut json_content = String::new();
+    file.read_to_string(&mut json_content)?;
+    let mut config: Config = serde_json::from_str(&json_content).unwrap();
 
-    println!("Do you know this kanji? (Y/N). Press O to open in full screen");
+    if let Some(category) = selected_category_arg {
+        if category != "all" {
+            let valid_categories: Vec<_> = config.kanji
+                .iter()
+                .map(|k| k.category.clone())
+                .collect();
+            if !valid_categories.contains(&category.to_string()) {
+                println!(
+                    "Invalid category specified. Available categories: {:?}",
+                    valid_categories
+                );
+                return Ok(());
+            }
+        }
+        config.selected_category = if category == "all" {
+            None
+        } else {
+            Some(category.to_string())
+        };
+
+        fs::write("kanji.json", serde_json::to_string_pretty(&config)?)?;
+    }
+
+    let filtered_kanji = match &config.selected_category {
+        Some(category) =>
+            config.kanji
+                .iter()
+                .filter(|k| &k.category == category)
+                .cloned()
+                .collect::<Vec<Kanji>>(),
+        None => config.kanji.clone(),
+    };
+
+    let random_kanji = &filtered_kanji[rand::thread_rng().gen_range(0..filtered_kanji.len())];
+
+    println!("Kanji: {}", random_kanji.character);
+    println!("Onyomi: {}", random_kanji.onyomi);
+    println!("Kunyomi: {}", random_kanji.kunyomi);
+
+    println!("Do you know this kanji? (Y/N). Press Q to quit, or O to open in full screen.");
 
     enable_raw_mode()?;
     let mut response = ' ';
@@ -71,8 +116,12 @@ fn main() -> std::io::Result<()> {
                 break;
             }
             KeyCode::Char('o') | KeyCode::Char('O') => {
-                show_kanji(&kanji.character)?;
+                show_kanji(&random_kanji.character)?;
                 continue;
+            }
+            KeyCode::Char('q') | KeyCode::Char('Q') => {
+                disable_raw_mode()?;
+                return Ok(());
             }
             _ => {
                 continue;
@@ -83,17 +132,17 @@ fn main() -> std::io::Result<()> {
 
     if response == 'y' {
         println!("What is the meaning of this kanji?");
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        let mut input_guess = String::new();
+        io::stdin().read_line(&mut input_guess)?;
 
-        if input.trim().eq_ignore_ascii_case(&kanji.meaning) {
+        if input_guess.trim().eq_ignore_ascii_case(&random_kanji.meaning) {
             println!("{}", "Correct!".green());
         } else {
             println!("{}", "Incorrect.".red());
-            println!("The correct meaning is: {}", kanji.meaning);
+            println!("The correct meaning is: {}", random_kanji.meaning);
         }
     } else if response == 'n' {
-        println!("The meaning of this kanji is: {}", kanji.meaning);
+        println!("The meaning of this kanji is: {}", random_kanji.meaning);
     }
 
     Ok(())
